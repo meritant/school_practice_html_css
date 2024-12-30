c198d5a42d06d285c9065e9647ac7048a9ef649e
Aleks C <useruser@192.168.1.103>
2024-12-30 16:36:02 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,124 @@
+/* Add any JavaScript you need to this file. */
+[
+  {
+    name: 'Pepper',
+    type: 'Hot',
+    description:
+      'Spicy Slice jalapeño peppers result from hybrid plant breeding, \
+    and produce numerous uniform, perfect green pods per plant. \
+    These will eventually mature to a sweeter red colour, \
+    but at the green stage they are pungent and hot — \
+    substantially hotter than Jalapeño M, but with \
+    the distinct flavour of jalapeño chiles. This \
+    product was something of a hit in our field trials, \
+    with plants positively laden with peppers. \
+    Everyone who tried it agreed on the superior \
+    flavour and genuine piquancy that runs throughout \
+    the flesh, not just restricted to the pith and seeds.',
+    price: 5,
+    cents: 99, // NOTE: prices are often done in whole cents and then formatted to dollars/cents
+    category: 'food', // category of this item
+    imageUrl: 'images/hotp.jpg' // relative URL to image of this item
+  },
+
+  {
+    name: 'Pepper',
+    type: 'Sweet',
+    description:
+      'Plant Orange Sun pepper seeds for striking, bright orange, sweet bell peppers.  \
+      Harvest as succulent, blocky green bell peppers, or wait until the fruits mature \
+       to high-vis orange. The 10-13cm (4-5") long fruits have thick flesh with three \
+        to four lobes, and really excellent flavour. This is another bell pepper that \
+         looks sensational in salads, and has a highly developed interior cavity for \
+          stuffing. The productive plants seldom exceed 60cm (24") in height, and work \
+           well in three to five gallon containers if the soil is fertile, moist, and \
+            freely draining.',
+    price: 7,
+    cents: 99, // NOTE: prices are often done in whole cents and then formatted to dollars/cents
+    category: 'food', // category of this item
+    imageUrl: 'images/sweetp.jpg' // relative URL to image of this item
+  },
+  {
+    name: 'Tomato',
+    type: 'Beefsteak',
+    description: '',
+    price: 8,
+    cents: 99, // NOTE: prices are often done in whole cents and then formatted to dollars/cents
+    category: 'food', // category of this item
+    imageUrl: 'images/sweetp.jpg' // relative URL to image of this item
+  },
+  {
+    name: 'Tomato',
+    type: 'Italian',
+    description: '',
+    price: 9,
+    cents: 99, // NOTE: prices are often done in whole cents and then formatted to dollars/cents
+    category: 'food', // category of this item
+    imageUrl: 'images/sweetp.jpg' // relative URL to image of this item
+  },
+  {
+    name: 'Tomato',
+    type: 'Cherry',
+    description: '',
+    price: 9,
+    cents: 99, // NOTE: prices are often done in whole cents and then formatted to dollars/cents
+    category: 'food', // category of this item
+    imageUrl: 'images/sweetp.jpg' // relative URL to image of this item
+  },
+  {
+    name: 'Lettuce',
+    type: 'Romaine',
+    description: '',
+    price: 9,
+    cents: 99, // NOTE: prices are often done in whole cents and then formatted to dollars/cents
+    category: 'food', // category of this item
+    imageUrl: 'images/let.jpg' // relative URL to image of this item
+  },
+  {
+    name: 'Lettuce',
+    type: 'Bibb',
+    description: '',
+    price: 9,
+    cents: 99, // NOTE: prices are often done in whole cents and then formatted to dollars/cents
+    category: 'food', // category of this item
+    imageUrl: 'images/let2.jpg' // relative URL to image of this item
+  },
+
+  {
+    name: 'Flowers',
+    type: 'Yellow',
+    description: '',
+    price: 9,
+    cents: 99, // NOTE: prices are often done in whole cents and then formatted to dollars/cents
+    category: 'home', // category of this item
+    imageUrl: 'images/sweetp.jpg' // relative URL to image of this item
+  },
+
+  {
+    name: 'Succulent',
+    type: 'Mexican',
+    description: '',
+    price: 9,
+    cents: 99, // NOTE: prices are often done in whole cents and then formatted to dollars/cents
+    category: 'home', // category of this item
+    imageUrl: 'images/sweetp.jpg' // relative URL to image of this item
+  },
+  {
+    name: 'Flowers',
+    type: 'Rose',
+    description: '',
+    price: 9,
+    cents: 99, // NOTE: prices are often done in whole cents and then formatted to dollars/cents
+    category: 'home', // category of this item
+    imageUrl: 'images/sweetp.jpg' // relative URL to image of this item
+  },
+  {
+    name: 'Flowers',
+    type: 'Calendula',
+    description: '',
+    price: 9,
+    cents: 99, // NOTE: prices are often done in whole cents and then formatted to dollars/cents
+    category: 'home', // category of this item
+    imageUrl: 'images/sweetp.jpg' // relative URL to image of this item
+  }
+];
